08eca23ed69c07bb688c69970017fcafc926d0e6
futuremirae <kml8486@naver.com>
2025-02-26 01:12:19 +0900

    [Chore] 프로젝트 생성

---
+++ SearcherProject/SearcherProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SearcherProject
+//
+//  Created by 김미래 on 2/26/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
