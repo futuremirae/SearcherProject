ba60c4981d3312166fa963ea737268c83e21be4a
futuremirae <kml8486@naver.com>
2025-02-26 21:21:55 +0900

    [Feat] 불필요한 에러타입 정리

--- SearcherProject/SearcherProject/Network/NetworkError.swift
+++ SearcherProject/SearcherProject/Network/NetworkError.swift
@@ -11,9 +11,7 @@ enum NetworkError: Error {
     case invalidURL
     case noData
     case statusCodeError
-    case invalidServerResponse
     case downloadImageError
-    case keychainError
 
     var errorDescription: String? {
         switch self {
@@ -23,12 +21,8 @@ enum NetworkError: Error {
             return "데이터가 존재하지 않습니다."
         case .statusCodeError:
             return "잘못된 응답입니다."
-        case .invalidServerResponse:
-            return "상태코드 에러(200이 아닙니다.)"
         case .downloadImageError:
             return "이미지 다운에 실패했습니다."
-        case .keychainError:
-            return "Keychain 에러"
         }
     }
 }
